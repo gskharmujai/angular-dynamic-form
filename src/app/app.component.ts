1b17ca16e2f195f941f0ae299d6e24af1fd5fbc0
George K <gskharmujai@gmail.com>
2023-03-17 03:38:32 +0530

    Implemented a form to add/delete fields dynamically

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
+  _locations = [
+    { 'id': '1', 'name': 'Shillong'},
+    { 'id': '2', 'name': 'Bangalore'},
+  ];
+  
+  _hotels = [
+    { 'id': '1', 'name': 'Pegasus'},
+    { 'id': '2', 'name': 'Broadway'},
+  ];
+  
   title = 'dynamic-form';
+  
+  productForm = this.fb.group({
+      name: [''],
+      quantities: this.fb.array([])
+  });
+  
+  constructor(private fb: FormBuilder) {
+    this.addQuantity();
+  }
+  
+  get quantities() {
+    return this.productForm.controls['quantities'] as FormArray;
+  }
+
+  addQuantity() {
+    const quantity = this.fb.group({
+      hotel: [''],
+      location: ['']
+    });
+    this.quantities.push(quantity);
+  }
+  
+  removeQuantity(i:number) {
+    this.quantities.removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value);
+  }
+
 }
